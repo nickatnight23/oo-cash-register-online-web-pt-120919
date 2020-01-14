4d0302f2f245fd3a4998212789def4785e5c5487
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-14 20:45:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,6 +8,14 @@ class CashRegister
     @items= []
   end
   
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
+  end
+  
   
   
   
